3b2eaa3847de3a1bac5deec1b7b1c812b09feb72
James Young <imjamesyoung@gmail.com>
2025-02-03 15:02:29 -0800

    setting up postgres and docker, add journal

---
+++ journal.md
@@ -0,0 +1,1 @@
+synchronize setting on postgres should be false in production - data loss & conflicts
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ServicesService } from './services/services.service';
 import { ServicesController } from './controllers/services.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Service } from './entities/service.entity';
+import { ServiceVersion } from './entities/service-version.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [Service, ServiceVersion],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [ServicesController],
   providers: [ServicesService],
 })
